3586a14a2486119d4e962a16cbba22e99a8ac9ec
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-11-19 21:18:08 -0500

    Add initial structure for Project.py with functions

---
+++ Project.py
@@ -0,0 +1,51 @@
+
+---
+
+#  **Project.py**
+
+```python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
+
+# Q1
+def plot_polynomial():
+    return
+
+# Q2
+def plot_compare_functions():
+    return
+
+# Q3
+def plot_array_from_txt(filename):
+    return
+
+# Q4
+def plot_monthly_temperature(filename):
+    return
+
+# Q5
+def plot_scatter_from_csv(filename):
+    return
+
+# Q6
+def plot_heatmap(filename):
+    return
+
+# Q7
+def is_invertible(filename):
+    return
+
+# Q8
+def are_orthogonal(A, B):
+    return
+
+# Q9
+def plot_surface():
+    return
+
+# Q10
+def animate_sine_wave():
+    return
